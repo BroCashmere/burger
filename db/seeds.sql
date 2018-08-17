166bcc08437336ea0dbff97ed765113624d60a79
unknown <eyangsd@gmail.com>
2018-08-16 20:06:41 -0700

    basic functionality working

---
+++ db/seeds.sql
@@ -0,0 +1,3 @@
+INSERT INTO burgers (burger_name) VALUES ('Double Double Burger');
+INSERT INTO burgers (burger_name) VALUES ('Double Char Burger');
+INSERT INTO burgers (burger_name) VALUES ('Double Whopper');
---
+++ public/assets/js/burgers.js
@@ -0,0 +1,45 @@
+$(function(){
+  
+  $('.change-devour').on('click', function(){
+        var id = $(this).data('id');
+        var newStatus = $(this).data("newStatus");
+
+        var newDevouredState = {
+            devoured: newStatus
+        };
+        // Send the PUT request.
+    $.ajax("/api/burgers/" + id, {
+        type: "PUT",
+        data: newDevouredState
+      }).then(
+        function() {
+          console.log("changed status to", newStatus);
+          // Reload the page to get the updated list
+          location.reload();
+        }
+      );
+    });
+  
+    $(".create-form").on("submit", function(event) {
+      // Make sure to preventDefault on a submit event.
+      event.preventDefault();
+  
+      var newBurger = {
+        burger_name: $("#burger").val(),
+        devoured: $("[burger_name=devoured]:checked").val()
+      };
+  
+      // Send the POST request.
+      $.ajax("/api/burgers", {
+        type: "POST",
+        data: newBurger
+      }).then(
+        function() {
+          console.log("created new burger");
+          // Reload the page to get the updated list
+          location.reload();
+        }
+      );
+    });
+
+  }) ; 
